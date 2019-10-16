3e085f5a384f91f2259ec36743fc81533121bccc
Daniel Florescu <Daniel.florescu1905@gmail.com>
2019-10-16 10:30:18 +0200

    added more

--- main.cpp
+++ main.cpp
@@ -1,39 +1,56 @@
 #include <iostream>
 #include <array>
 
-int decks;
-std::array<int, 12> deck;
-int currentDeck[12];
-
-void multiplyArr(int* arr, int num){
-
-}
+int decks = 1;
+std::array<int, 12> currentDeck;
 
 void generateDecks(){
-  deck = {
-    4, // ace
+  currentDeck = {
+    4*decks, // ace
     0, // 1 is not existing
-    4, // 2 -
-    4,
-    4,
-    4,
-    4,
-    4,
-    4,
-    4,
-    4, // - 10
-    12
+    4*decks, // 2 -
+    4*decks,
+    4*decks,
+    4*decks,
+    4*decks,
+    4*decks,
+    4*decks,
+    4*decks,
+    4*decks, // - 10
+    12*decks
   };
 }
 
+void removeCard(int card){
+  currentDeck[card] = currentDeck[card]-1;
+}
+
+void ai(){
+
+}
+
+void startup(int people){ // The start of a poker game has special rules.
+  int card;
+  std::cout << "Specify card for each player.";
+  for(int n = 1; n < people; n++){
+    std::cin >> card;
+    removeCard(card);
+  }
+}
+
 void userInterface1(){
+  int people = 0;
+
   std::cout << "How many decks?\n";
   std::cin >> decks;
+  std::cout << "How many people?\n";
+  std::cin >> people;
+  generateDecks();
 
 }
 
 int main() 
 {
-  std::cout << "Hello, World!";
+  userInterface1();
   return 0;
 }
